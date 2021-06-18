69e64ea9f19d38c5ffe3c7486f01adaea8475a65
NickPuchko <puchko2000n@gmail.com>
2021-06-18 11:33:31 +0300

    Apple-MVC module template

---
+++ Apple-MVC.xctemplate/___FILEBASENAME___/___FILEBASENAME___Model.swift
@@ -0,0 +1,19 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import Foundation
+import UIKit
+
+class ___FILEBASENAME___Model {
+	weak var viewController: ___FILEBASENAME___ViewController!
+    
+	init(_ viewController: ___FILEBASENAME___ViewController) {
+		self.viewController = viewController
+    }
+    
+}
---
+++ Apple-MVC.xctemplate/___FILEBASENAME___/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+class ___FILEBASENAME___ViewController: UIViewController {
+    private lazy var model = ___FILEBASENAME___Model(self)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
